7820d6623a6f99880f34df3f5d3f0d8ace8f309e
Pablo Santiago Semhan <santiagosemhan@gmail.com>
2017-03-15 20:27:11 -0300

    Instalación de bundles. Aseguramiento de la app. Configuración inicial.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new AdminBundle\AdminBundle(),
+            new FrontBundle\FrontBundle(),
+            new UserBundle\UserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
