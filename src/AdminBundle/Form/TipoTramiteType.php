b20f2c4b810735493d5da9a0f939e44bd82e1918
Pablo Santiago Semhan <santiagosemhan@gmail.com>
2018-01-30 12:34:36 -0300

    Fix import FormEvent

--- src/AdminBundle/Form/TipoTramiteType.php
+++ src/AdminBundle/Form/TipoTramiteType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
